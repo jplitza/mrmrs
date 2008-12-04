c3254bcb74188fee5cc521697cc0b86597e09db0
Jan-Philipp Litza <janphilipp@litza.de>
2008-12-04 11:19:47 +0100

    fix for last commit

--- admin.php
+++ admin.php
@@ -8,8 +8,9 @@ $prefix = 'mrmrs_';
 
 if(empty($_GET['page']))
   $t->display('admin_overview.tpl');
-elseif(is_file('templates/'.basename($_GET['page']).'.tpl') && is_file(basename('acp_pages/'.$_GET['page']).'.php'))
+elseif(is_file('templates/'.basename($_GET['page']).'.tpl') && is_file('acp_pages/'.basename($_GET['page']).'.php'))
 {
+  echo 'foo';
   require('acp_pages/'.basename($_GET['page']).'.php');
   $t->display(basename($_GET['page']).'.tpl');
 }
